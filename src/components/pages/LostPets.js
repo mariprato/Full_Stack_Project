201e43f1b6e7c65d77b1eafc8d3789662e9c7178
Emma Holden <emmaholden91@yahoo.com>
2024-05-14 16:11:28 +0100

    added map to go through array

--- src/components/pages/LostPets.js
+++ src/components/pages/LostPets.js
@@ -4,15 +4,17 @@ import pets from "../../petDatabase.js"
 import './LostPets.css';
 
 const LostPets = () => {
-
+  console.log(pets)
   return (
     <div className = "lost-pets-layout">
-      <h1>This is the Lost Pets page</h1>
-      {/* This will be removed and the pets array will be mapped through. Putting this here to demonstrate the component as page logic does not yet exist. */}
+      <h1>Lost Pets</h1>
       <div className = "petCardsContainer">
-        <PetCard pet = {pets[0]} />
-        <PetCard pet = {pets[1]} />
+      {pets.map((pet) => 
+          <PetCard key={pet.id} pet={pet} />
+      )}
       </div>
+      
+
     </div>
   );
 };
