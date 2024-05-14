2cd3de811a4f28b4adb123f62cc03da48e601e7f
Amiker Best <best.amiker@gmail.com>
2024-05-14 11:15:40 +0100

    navbar design completed

--- src/components/layout/navbar.js
+++ src/components/layout/navbar.js
@@ -1,7 +1,15 @@
 import React from "react";
 import "./navbar.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import {
+  faFacebookF,
+  faInstagram,
+  faPinterestP,
+  faSquareFacebook,
+  faThreads,
+} from "@fortawesome/free-brands-svg-icons";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 
 const Navbar = () => {
   return (
@@ -9,34 +17,47 @@ const Navbar = () => {
       <div className="top-nav">
         <div className="left-section">
           <div className="navbar-symbol">
-            <FontAwesomeIcon icon={faInstagram} />
+            <FontAwesomeIcon className="contact-icons" icon={faInstagram} />
           </div>
-          <div className="navbar-symbol">Symbol 2</div>
-          <div className="navbar-symbol">Symbol 3</div>
+          <div className="navbar-symbol">
+            <FontAwesomeIcon className="contact-icons" icon={faFacebookF} />
+          </div>
+          <div className="navbar-symbol">
+            <FontAwesomeIcon className="contact-icons" icon={faThreads} />
+          </div>
+          <div className="navbar-symbol">
+            <FontAwesomeIcon className="contact-icons" icon={faPinterestP} />
+          </div>
         </div>
         <div className="right-section">
-          <div className="navbar-contact">phone number here</div>
-          <a
-            className="navbar-contact"
-            href="mailto:best.amiker@gmail.com"
-            rel="noreferrer"
-            target="_blank"
-          >
-            email@mail.com
-          </a>
+          <div className="navbar-contact">
+            <FontAwesomeIcon className="contact-icons" icon={faPhone} />
+            (000) 000 000
+          </div>
+          <div className="navbar-contact">
+            <a
+              className="navbar-email"
+              href="mailto:enquiry@fur-everfound.com"
+              rel="noreferrer"
+              target="_blank"
+            >
+              {" "}
+              <FontAwesomeIcon className="contact-icons" icon={faEnvelope} />
+              enquiry@fur-everfound.com
+            </a>
+          </div>
         </div>
       </div>
       <div className="overlay">
         <div className="logo">
           <img src="/image/MagicEraser_240512_152556 2.png" />
         </div>
-        <div className="page-links">
-          <a>one</a>
-          <a>one</a>
-          <a>one</a>
-          <a>one</a>
-          <a>one</a>
-          <button>button</button>
+        <div className="overlay-right">
+          <a className="overlay-links">Home</a>
+          <a className="overlay-links">Lost Pets</a>
+          <a className="overlay-links">Details</a>
+          <a className="overlay-links">Contact Us</a>
+          <button className="primary-button">button</button>
         </div>
       </div>
     </nav>
