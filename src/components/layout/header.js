c8d2c5795b2943a6726249d5c23144d68265a721
unknown <laura.jim.hernandez@gmail.com>
2024-05-14 16:41:37 +0100

    Created Header.js and Header.css files. Added header image to images. Created basic Header.js skeleton

---
+++ src/components/layout/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css'
+function Header() {
+    return (
+
+        <div className="header">
+        <h1>Fur-ever Found</h1>
+        <h2>Bringing lost pets back to their loving arms</h2>
+        <img src="Header.jpeg" alt="Header Image" />
+
+    </div>
+
+    )
+}
+
+export default Header
