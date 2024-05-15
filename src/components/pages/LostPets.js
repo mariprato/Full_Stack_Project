2850dff21484aa91f8be402f1a3d154c688913bf
Emma Holden <emmaholden91@yahoo.com>
2024-05-15 14:48:36 +0100

    Changes to lost pets page to only render 8 cards on each page

--- src/components/pages/LostPets.js
+++ src/components/pages/LostPets.js
@@ -1,20 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PetCard from "../PetCard";
 import pets from "../../petDatabase.js"
 import './LostPets.css';
+import Pagination from "../layout/pagination.js";
 
 const LostPets = () => {
-  console.log(pets)
+  function getPetArray(pets){
+    const indexToStart = (activePage - 1) * 8;
+    return pets.slice(indexToStart, indexToStart + 8) 
+  }
+
+  const [activePage, setActivePage] = useState(1)
+  const [petsToDisplay, setPetsToDisplay] = useState([])
+
+  useEffect(() => {
+    setPetsToDisplay([])
+    const petArray = getPetArray(pets)
+    setPetsToDisplay(petArray)
+  }, [activePage]);
+  
   return (
     <div className = "lost-pets-layout">
       <h1>Lost Pets</h1>
       <div className = "petCardsContainer">
-      {pets.map((pet) => 
+      {petsToDisplay.map((pet) => 
           <PetCard key={pet.id} pet={pet} />
       )}
       </div>
       
-
+      <Pagination pets={pets} activePage={activePage} setActivePage={setActivePage} setPetsToDisplay={setPetsToDisplay}/>
     </div>
   );
 };
