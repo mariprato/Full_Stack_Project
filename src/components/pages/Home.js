e2e5073e6e92bbf87ac6deca9db6e2bbdfc74c0d
abbyunderwood <137214884+abbyunderwood@users.noreply.github.com>
2024-05-18 11:29:12 +0100

    Footer with newsletter

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../layout/navbar";
+import Header from "../layout/header";
 import Footer from "../layout/footer";
 
 const Home = () => {
   return (
     <>
       <Navbar />
+      <Header />
       <div>
         <h1>This is the home page</h1>
       </div>
