3116fd3e4a3480caad960326cf65d4ef0d9ece79
Amiker Best <best.amiker@gmail.com>
2024-05-20 16:39:48 +0100

    title image added to pages and navbar colour change

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,13 +1,18 @@
 import FooterCom from "./footer";
 import Navbar from "./navbar";
 import "./Layout.css";
+import { useLocation } from "react-router-dom";
 
 const Layout = (props) => {
   const { children } = props;
+  const location = useLocation();
+  const isHomePage = location.pathname === "/";
   return (
     <>
       <Navbar />
-      <div className="layout-page">{children}</div>
+      <div className={`layout-page ${isHomePage ? "" : "not-home-page"}`}>
+        {children}
+      </div>
       <FooterCom />
     </>
   );
