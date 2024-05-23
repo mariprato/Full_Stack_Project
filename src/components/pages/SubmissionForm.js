fbb79a5bd3d78c49375bec3ab470ac55353727c6
abbyunderwood <137214884+abbyunderwood@users.noreply.github.com>
2024-05-23 14:26:34 +0100

    Added layout of the page - includes navbar and footer

--- src/components/pages/SubmissionForm.js
+++ src/components/pages/SubmissionForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import Navbar from "../layout/navbar";
 import './SubmissionForm.css';
 import ButtonComponent from "../ButtonComponent";
+import Layout from "../layout/Layout.js";
 
 const SubmissionForm = () => {
   const [formData, setFormData] = useState({
@@ -71,106 +71,107 @@ const SubmissionForm = () => {
 
   return (
     <>
-      <Navbar />
-      <div className="submission-form-container">
-        <h1>Post Your Lost Pet Now</h1>
-        {confirmationMessage && (
-          <div className="confirmation-message">
-            {confirmationMessage}
-          </div>
-        )}
-        <form className="submission-form" onSubmit={handleSubmit}>
-          <div className="form-group image-upload">
-            <label>
-              <span>Browse</span>
+      <Layout>
+        <div className="submission-form-container">
+          <h1>Post Your Lost Pet Now</h1>
+          {confirmationMessage && (
+            <div className="confirmation-message">
+              {confirmationMessage}
+            </div>
+          )}
+          <form className="submission-form" onSubmit={handleSubmit}>
+            <div className="form-group image-upload">
+              <label>
+                <span>Browse</span>
+                <input
+                  type="file"
+                  name="image"
+                  accept="image/*"
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
+            <div className="form-group">
               <input
-                type="file"
-                name="image"
-                accept="image/*"
+                type="text"
+                placeholder="Your Name"
+                name="name"
+                value={formData.name}
                 onChange={handleChange}
+                required
+                className={formData.name.trim() === "" ? "touched" : ""}
               />
-            </label>
-          </div>
-          <div className="form-group">
-            <input
-              type="text"
-              placeholder="Your Name"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-              required
-              className={formData.name.trim() === "" ? "touched" : ""}
-            />
-          </div>
-          <div className="form-group">
-            <input
-              type="email"
-              placeholder="Your Email Address"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              required
-              className={formData.email.trim() === "" ? "touched" : ""}
-            />
-          </div>
-          <div className="form-group">
-            <input
-              type="text"
-              placeholder="Pet's Name"
-              name="petName"
-              value={formData.petName}
-              onChange={handleChange}
-              required
-              className={formData.petName.trim() === "" ? "touched" : ""}
-            />
-          </div>
-          <div className="form-group">
-            <input
-              type="date"
-              placeholder="Date Lost"
-              name="dateLost"
-              value={formData.dateLost}
-              onChange={handleChange}
-              required
-              max={new Date().toISOString().split('T')[0]} // Set max attribute to current date
-              className={formData.dateLost.trim() === "" ? "touched" : ""}
-            />
-          </div>
-          <div className="form-group">
-            <input
-              type="text"
-              placeholder="Location Lost"
-              name="locationLost"
-              value={formData.locationLost}
-              onChange={handleChange}
-              required
-              className={formData.locationLost.trim() === "" ? "touched" : ""}
-            />
-          </div>
-          <div className="form-group">
-            <textarea
-              placeholder="Additional Details"
-              name="additionalDetails"
-              rows="4"
-              value={formData.additionalDetails}
-              onChange={handleChange}
-              required
-              className={formData.additionalDetails.trim() === "" ? "touched" : ""}
-            ></textarea>
-          </div>
-          <div className="form-group">
-            <ButtonComponent type="submit" variant="button-form-submit">Submit</ButtonComponent>
-          </div>
-        </form>
-        <div className="contact-info">
-          <div className="contact-item">
-            <i className="fa fa-phone"></i> (000) 000 000
-          </div>
-          <div className="contact-item">
-            <i className="fa fa-envelope"></i> info@fur-everfound.com
+            </div>
+            <div className="form-group">
+              <input
+                type="email"
+                placeholder="Your Email Address"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+                className={formData.email.trim() === "" ? "touched" : ""}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="text"
+                placeholder="Pet's Name"
+                name="petName"
+                value={formData.petName}
+                onChange={handleChange}
+                required
+                className={formData.petName.trim() === "" ? "touched" : ""}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="date"
+                placeholder="Date Lost"
+                name="dateLost"
+                value={formData.dateLost}
+                onChange={handleChange}
+                required
+                max={new Date().toISOString().split('T')[0]} // Set max attribute to current date
+                className={formData.dateLost.trim() === "" ? "touched" : ""}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="text"
+                placeholder="Location Lost"
+                name="locationLost"
+                value={formData.locationLost}
+                onChange={handleChange}
+                required
+                className={formData.locationLost.trim() === "" ? "touched" : ""}
+              />
+            </div>
+            <div className="form-group">
+              <textarea
+                placeholder="Additional Details"
+                name="additionalDetails"
+                rows="4"
+                value={formData.additionalDetails}
+                onChange={handleChange}
+                required
+                className={formData.additionalDetails.trim() === "" ? "touched" : ""}
+              ></textarea>
+            </div>
+            <div className="form-group">
+              <ButtonComponent type="submit" variant="button-form-submit">Submit</ButtonComponent>
+            </div>
+          </form>
+          <div className="contact-info">
+            <div className="contact-item">
+              <i className="fa fa-phone"></i> (000) 000 000
+            </div>
+            <div className="contact-item">
+              <i className="fa fa-envelope"></i> info@fur-everfound.com
+            </div>
           </div>
         </div>
-      </div>
+      </Layout>
     </>
   );
 };
