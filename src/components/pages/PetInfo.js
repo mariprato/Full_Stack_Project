c2ebf1d7318d832b6c5560e8e41a367d58f86a37
Emma Holden <emmaholden91@yahoo.com>
2024-05-23 11:53:43 +0100

    remove blue line under pet names

--- src/components/pages/PetInfo.js
+++ src/components/pages/PetInfo.js
@@ -17,12 +17,7 @@ const PetInfo = () => {
           <h1>Details</h1>
           <div className="pet-info-container">
               <div className = "pet-info-image-container">
-                    <div className="pet-info-background-image">
-                        <img className="pet-info-image" src={`../images/${pet.id}.jpg`}></img>
-                    </div>
-                    <div className="pet-info-foreground-image">
-                        <img className = "pet-info-image" src={`../images/${pet.id}.jpg`} alt = {`Image of ${pet.name} the lost ${pet.type}.`}></img>
-                    </div>
+                  <img className = "pet-info-image" src={`../images/${pet.id}.jpg`} alt = {`${pet.name} the lost ${pet.type}.`}></img>
               </div>
             <div className = "pet-info-details-container">
               <p><span className="pet-info-details-keys">Pet's name: </span><span className="pet-info-details-values">{pet.name}</span></p>
