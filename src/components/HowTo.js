4778d3a48b69dcd58bbf934f4906292dc516314c
Amiker Best <best.amiker@gmail.com>
2024-05-24 11:09:03 +0100

    how to section added to home page

---
+++ src/components/HowTo.js
@@ -0,0 +1,15 @@
+import "./HowTo.css";
+
+const HowTo = () => {
+  return (
+    <div>
+      <h1>How Fur-Ever Found Works</h1>
+      <div className="how-container">
+        <div className="how-top">top</div>
+        <div className="how-bottom">bottom</div>
+      </div>
+    </div>
+  );
+};
+
+export default HowTo;
