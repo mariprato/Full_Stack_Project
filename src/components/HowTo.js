ec81e3ff1462397c183a4fda50b1567ad928aa58
Amiker Best <best.amiker@gmail.com>
2024-05-24 19:58:25 +0100

    downwards arrow to scroll to how to section

--- src/components/HowTo.js
+++ src/components/HowTo.js
@@ -2,11 +2,17 @@ import "./HowTo.css";
 
 const HowTo = () => {
   return (
-    <div>
+    <div id="how-to-section">
       <h1>How Fur-Ever Found Works</h1>
       <div className="how-container">
-        <div className="how-top">top</div>
-        <div className="how-bottom">bottom</div>
+        <div className="how-top">
+          <h1>My Pet Is Lost</h1>
+          <img src="/images/LostPet.jpg" />
+        </div>
+        <div className="how-bottom">
+          <h1>I Have Seen a Lost Pet</h1>
+          <img src="/images/LostPet.jpg" />
+        </div>
       </div>
     </div>
   );
