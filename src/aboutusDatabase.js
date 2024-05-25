3a0f5d0d49b0a9d68b3dd3289c9fdfcccb412cca
Laura Jimenez <laura.jim.hernandez@gmail.com>
2024-05-25 12:14:10 +0200

    Added individual AI images to AboutUs page

--- src/aboutusDatabase.js
+++ src/aboutusDatabase.js
@@ -3,31 +3,31 @@ const colleague = [
     {
       id: 1,
       name: "Abby",
-      image: "../images/SampleColleague.jpg",
-      details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Etiam non quam lacus suspendisse faucibus interdum posuere lorem. Sed velit dignissim sodales ut eu sem. Elit eget gravida cum sociis",
+      image: "../images/Abby.png",
+      details: "HLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Etiam non quam lacus suspendisse faucibus interdum posuere lorem. Sed velit dignissim sodales ut eu sem. Elit eget gravida cum sociis",
     },
     {
       id: 2,
       name: "Amiker",
-      image: "../images/SampleColleague.jpg",
+      image: "../images/Amiker.png",
       details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Etiam non quam lacus suspendisse faucibus interdum posuere lorem. Sed velit dignissim sodales ut eu sem. Elit eget gravida cum sociis",
     },
     {
       id: 3,
       name: "Emma",
-      image: "../images/SampleColleague.jpg",
+      image: "../images/Emma.png",
       details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Etiam non quam lacus suspendisse faucibus interdum posuere lorem. Sed velit dignissim sodales ut eu sem. Elit eget gravida cum sociis",
     },
     {
       id: 4,
       name: "Mariane",
-      image: "../images/SampleColleague.jpg",
+      image: "../images/Mariane.png",
       details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Etiam non quam lacus suspendisse faucibus interdum posuere lorem. Sed velit dignissim sodales ut eu sem. Elit eget gravida cum sociis",
     },
     {
       id: 5,
       name: "Laura",
-      image: "../images/SampleColleague.jpg",
+      image: "../images/Laura.png",
       details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Etiam non quam lacus suspendisse faucibus interdum posuere lorem. Sed velit dignissim sodales ut eu sem. Elit eget gravida cum sociis",
     },
   ];
