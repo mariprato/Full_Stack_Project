66cc7c310a865047de230dbe1b9e4c2075728381
Laura Jimenez <laura.jim.hernandez@gmail.com>
2024-05-27 20:46:43 +0200

    Added individual profile descriptions

--- src/aboutusDatabase.js
+++ src/aboutusDatabase.js
@@ -4,31 +4,31 @@ const colleague = [
       id: 1,
       name: "Abby",
       image: "../images/Abby.png",
-      details: "HLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Etiam non quam lacus suspendisse faucibus interdum posuere lorem. Sed velit dignissim sodales ut eu sem. Elit eget gravida cum sociis",
+      details: "Hey there! I'm Abby, the Chief Whisker Whisperer at Fur-ever Found. I blend my love for animals with my coding prowess to help reunite lost pets with their families. When I'm not wrangling code, you'll find me surrounded by my furry companions, Whiskers and Muffin, or volunteering at the local animal shelter",
     },
     {
       id: 2,
       name: "Amiker",
       image: "../images/Amiker.png",
-      details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Etiam non quam lacus suspendisse faucibus interdum posuere lorem. Sed velit dignissim sodales ut eu sem. Elit eget gravida cum sociis",
+      details: "Hello! I'm Amiker, the creative force behind Fur-ever Found. I use my artistic skills to create captivating designs and content that make our platform stand out. In my free time, I love capturing the beauty of pets through photography, and my golden retriever, Daisy, is often my star model.",
     },
     {
       id: 3,
       name: "Emma",
       image: "../images/Emma.png",
-      details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Etiam non quam lacus suspendisse faucibus interdum posuere lorem. Sed velit dignissim sodales ut eu sem. Elit eget gravida cum sociis",
+      details: "Hey there! I'm Emma, the code ninja at Fur-ever Found. I craft flawless code to keep our platform purring. When I'm not on a coding spree, you'll find me perfecting my downward dog in yoga class. My cat, Sir Pounce-a-Lot, loves to join in by turning my yoga mat into his personal playground.",
     },
     {
       id: 4,
       name: "Mariane",
       image: "../images/Mariane.png",
-      details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Etiam non quam lacus suspendisse faucibus interdum posuere lorem. Sed velit dignissim sodales ut eu sem. Elit eget gravida cum sociis",
+      details: "Greetings! I'm Mariane, the data wizard at Fur-ever Found. I decode patterns to help bring lost pets home. When I’m not diving into data, I'm experimenting with wild new recipes. My dog, Chewbacca, is my enthusiastic taste-tester and sous-chef, always ready for the next culinary adventure!",
     },
     {
       id: 5,
       name: "Laura",
       image: "../images/Laura.png",
-      details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Etiam non quam lacus suspendisse faucibus interdum posuere lorem. Sed velit dignissim sodales ut eu sem. Elit eget gravida cum sociis",
+      details: "Hi! I'm Laura, the tech guru at Fur-ever Found. I fuse my love for technology with my passion for pets to help reunite lost fur-babies with their families. When I’m not coding, you’ll find me scaling cliffs or exploring the wilderness with my dog, Rocket. Adventure awaits!",
     },
   ];
   
