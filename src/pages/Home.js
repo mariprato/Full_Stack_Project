89228263f0182112fe269879ccb4fb761f2764a8
Emma Holden <emmaholden91@yahoo.com>
2024-05-28 19:46:15 +0100

    added header tests

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "../components/layout/header";
+import Header from "../components/layout/header/header";
 import Layout from "../components/layout/Layout";
 import HowTo from "../components/info/HowTo";
 
