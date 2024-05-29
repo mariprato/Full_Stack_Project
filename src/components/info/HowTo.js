ed1a6ddc2b2b5b3365961dc376fa006a66925506
Amiker Best <best.amiker@gmail.com>
2024-05-29 11:10:32 +0100

    alt tags added to the diagrams

--- src/components/info/HowTo.js
+++ src/components/info/HowTo.js
@@ -33,7 +33,7 @@ const HowTo = () => {
           <img
             className="how-top-img"
             src="/images/HowInfoDiagram.png"
-            alt=""
+            alt="Sequential cartoon-style depiction of the lost pet website process. Includes a person submitting pet details, a team of moderators reviewing submissions, a webpage displaying shared pet information, and a smartphone notification for found pets."
           />
           <ButtonComponent
             variant="button-post-pink"
@@ -47,7 +47,7 @@ const HowTo = () => {
           <img
             className="how-bottom-img"
             src="/images/HowInfoDiagram2.png"
-            alt=""
+            alt="Sequential cartoon-style depiction of the process for reporting a lost pet sighting. Includes a person spotting a lost pet, visiting the website to click the 'Contact Us' button, and the team quickly notifying the pet's owner."
           />
           <ButtonComponent variant="button-post-pink" onClick={goToLostPets}>
             Lost Pets
