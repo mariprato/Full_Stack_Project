02aaa423878b44363dfb8ee61cb72a8a0bbd501b
Amiker Best <best.amiker@gmail.com>
2024-05-31 12:49:53 +0100

    autocomplete bug fixed by adding optional chaining

--- src/services/googleMapsService.js
+++ src/services/googleMapsService.js
@@ -1,35 +1,39 @@
 class googleMapsService {
-    constructor() {
-      this.initializeService();
-    }
-  
-    initializeService() {
-      if (window.google && window.google.maps) {
-        this.autocompleteService = new window.google.maps.places.AutocompleteService();
-      } else {
-        window.initMap = () => {
-          this.autocompleteService = new window.google.maps.places.AutocompleteService();
-        };
-      }
-    }
-  
-    initializeAutocomplete(inputRef, onPlaceChanged) {
-      const autocomplete = new window.google.maps.places.Autocomplete(inputRef.current, {
-        types: ["address"],
-      });
-  
-      autocomplete.addListener("place_changed", () => {
-        const place = autocomplete.getPlace();
-        if (!place || !place.formatted_address) {
-          onPlaceChanged(null, new Error("No place details available."));
-        } else {
-          onPlaceChanged(place.formatted_address, null);
-        }
-      });
-  
-      return autocomplete;
+  constructor() {
+    this.initializeService();
+  }
+
+  initializeService() {
+    if (window?.google?.maps?.places) {
+      this.autocompleteService =
+        new window.google.maps.places.AutocompleteService();
+    } else {
+      window.initMap = () => {
+        this.autocompleteService =
+          new window.google.maps.places.AutocompleteService();
+      };
     }
   }
-  
-  export default googleMapsService;
-  
+
+  initializeAutocomplete(inputRef, onPlaceChanged) {
+    const autocomplete = new window.google.maps.places.Autocomplete(
+      inputRef.current,
+      {
+        types: ["address"],
+      }
+    );
+
+    autocomplete.addListener("place_changed", () => {
+      const place = autocomplete.getPlace();
+      if (!place || !place.formatted_address) {
+        onPlaceChanged(null, new Error("No place details available."));
+      } else {
+        onPlaceChanged(place.formatted_address, null);
+      }
+    });
+
+    return autocomplete;
+  }
+}
+
+export default googleMapsService;
