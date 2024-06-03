5a07d49d63f02be227e757ce77122b912b504f96
abbyunderwood <137214884+abbyunderwood@users.noreply.github.com>
2024-06-03 09:59:32 +0100

    How to tests (#61)

--- src/components/info/HowTo/HowTo.test.js
+++ src/components/info/HowTo/HowTo.test.js
@@ -12,18 +12,33 @@ describe('<HowTo/>', () => {
 
 
     test('should render the header', () => {
-        render(<HowTo/>);
-        const petLostHeader = screen.getByText(/My Pet Is Lost/i);
-        const foundPetHeader = screen.getByText(/I Have Seen a Lost Pet/i);
-        expect(petLostHeader).toBeInTheDocument();
-        expect(foundPetHeader).toBeInTheDocument();
-        });
-
-    test('should call useNavigate when button clicked', () => {
       render(<HowTo/>);
-      const postNowButton = screen.getByRole('button', { name: "Lost Pets"})
+      const petLostHeader = screen.getByText(/My Pet Is Lost/i);
+      const foundPetHeader = screen.getByText(/I Have Seen a Lost Pet/i);
+      expect(petLostHeader).toBeInTheDocument();
+      expect(foundPetHeader).toBeInTheDocument();
+      });
+
+    test('should call useNavigate when Post Now button is clicked', () => {
+      render(<HowTo/>);
+      const postNowButton = screen.getByRole('button', { name: "Post Now"})
       fireEvent.click(postNowButton);
       expect(mockedUseNavigate).toHaveBeenCalled();
+      });
+
+    test('should call useNavigate when Lost Pets button is clicked', () => {
+      render(<HowTo/>);
+      const LostPetsButton = screen.getByRole('button', { name: "Lost Pets"})
+      fireEvent.click(LostPetsButton);
+      expect(mockedUseNavigate).toHaveBeenCalled();
+      });
+
+    test('should render images with correct alt text', () => {
+      render(<HowTo/>);
+      const lostPetImg = screen.getByAltText(/Sequential cartoon-style depiction of the lost pet website process. Includes a person submitting pet details, a team of moderators reviewing submissions, a webpage displaying shared pet information, and a smartphone notification for found pets./i);
+      const seenPetImg = screen.getByAltText(/Sequential cartoon-style depiction of the process for reporting a lost pet sighting. Includes a person spotting a lost pet, visiting the website to click the 'Contact Us' button, and the team quickly notifying the pet's owner./i);
+      expect(lostPetImg).toBeInTheDocument();
+      expect(seenPetImg).toBeInTheDocument();
       });
 
 })
